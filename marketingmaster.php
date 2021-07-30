73f87bb8b74a848bca34fc94aaf6f34e2fd6035d
Michael Hicks <kustomdesigner@gmail.com>
2021-07-30 17:45:58 -0500

    First commit

--- marketingmaster.php
+++ marketingmaster.php
@@ -1,27 +1,32 @@
-<?php 
-/*
+<?php
+/* 
 *  Plugin Name: Marketing Master
 *  Plugin URI: https://kustomdeveloper.com/marketing-master
-*  Description: A Local SEO plugin that actually works. Features include: business schema, google maps, video schema, customer reviews and customer reviews schema.
+*  Description: A Local SEO plugin that actually works. Features: Business schema, google maps, video schema, customer reviews and customer reviews schema.
 *  Version: 1.0
 *  Author: Michael Hicks
 *  Author URI: https://michaelhicks.me
 *  License: GPLv2
 */
 
+/*
+*  Activate plugin
+*  -set default options
+*/
 register_activation_hook(__FILE__, 'markmast_install');
+function markmast_install() {
 
-function markmast() {
-  //Add tables
-
-
-}
-
-register_deactivation_hook(__FILE__, 'markmast_uninstall');
-
-function markmast_uninstall() {
-  // Remove tables
-  
+  // Add tables
 
 }
 
+
+/*
+*  Deactivate plugin
+*  -do somethign
+*/
+register_deactivation_hook(__FILE__, 'markmast_deactivate');
+function markmast_deactivate() {
+  // Deactivate something here
+
+}
---
+++ uninstall.php
@@ -0,0 +1,6 @@
+<?php
+// If uninstall not called from wordpress exit
+if( !defined('WP_UNINSTALL_PLUGIN') ) 
+  exit();
+
+// Remove any other tables or options here
