34a5b26c688a41aa98f1636097c73fcca64aae02
Michael Hicks <kustomdesigner@gmail.com>
2021-08-13 12:55:56 -0500

    Creating local business schema options

--- includes/markmast_options_settings.php
+++ includes/markmast_options_settings.php
@@ -1,24 +1,73 @@
-<h1>Marketing Master Settings</h1>
+<div id="markmast-main-options">
 
-<div class="switchbox checkbox-1">
-  <label class="switch">
-    <input type="checkbox" name="box1">
-    <span class="slider round"></span>
-  </label>
-</div>
+  <h1>Marketing Master Settings</h1>
 
-<div class="switchbox checkbox-2">
-  <label class="switch">
-    <input type="checkbox" name="box2">
-    <span class="slider round"></span>
-  </label>
-</div>
+  <div class="switchbox checkbox-1">
+  <h3>Local Business Schema</h3>
+    <label class="switch">
+      <input type="checkbox" name="box1">
+      <span class="slider round"></span>
+    </label>
 
-<div class="switchbox checkbox-3">
-  <label class="switch">
-    <input type="checkbox" name="box3">
-    <span class="slider round"></span>
-  </label>
+    <div class="local-business">
+      <label for="Business Name" class="b-label">Business Name*
+        <input class="input-block" type="text" name="local-business-name" required>
+      </label>
+
+      <label for="Business Image" class="b-label">Business Img*
+        <input class="input-block" type="text" name="local-business-img" required>
+      </label>
+
+      <label for="Business Phone"  class="b-label">Business Phone*
+        <input class="input-block" type="text" name="local-business-phone" required>
+      </label>
+
+      <label for="Business Logo" class="b-label">Business Logo*
+        <input class="input-block" type="text" name="local-business-logo" required>
+      </label>
+
+      <label for="Business Desription" class="b-label">Business Description*
+        <input class="input-block" type="text" name="local-business-description" required>
+      </label>
+
+      <label for="Business Hours" class="b-label">Business Hours <span class="openhours-example">(default: Su,Mo,Tu,We,Th,Fr,Sa 00:00-24:00)</span>
+        <input class="input-block" type="text" name="local-business-hours" required>
+      </label>
+
+      <h3>Address</h3>
+      <label for="Business " class="b-label">Business Street
+        <input class="input-block" type="text" name="local-business-street-address" required>
+      </label>
+
+      <label for="Business City" class="b-label">Business City
+        <input class="input-block" type="text" name="local-business-city" required>
+      </label>
+
+      <label for="Business State" class="b-label">Business State
+        <input class="input-block" type="text" name="local-business-state" required>
+      </label>
+
+      <label for="Business Zip" class="b-label">Business Zip
+        <input class="input-block" type="text" name="local-business-zip" required>
+      </label>  
+    </div>
+
+  </div>
+
+  <div class="switchbox checkbox-2">
+    <label class="switch">
+      <input type="checkbox" name="box2">
+      <span class="slider round"></span>
+    </label>
+  </div>
+
+  <div class="switchbox checkbox-3">
+    <label class="switch">
+      <input type="checkbox" name="box3">
+      <span class="slider round"></span>
+    </label>
+  </div>
+
 </div>
 
 
